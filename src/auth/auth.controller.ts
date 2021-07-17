b3f58cbfe74c7277261aec967d287e18f1b13f84
oldcryptogeek <kodex.data@gmail.com>
2021-07-17 15:53:23 -0600

    implement uuidv4 for userID

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,41 +1,54 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { JwtService } from '@nestjs/jwt';
-import { Response } from 'express';
-
-@Controller('auth')
+import { Controller, Get, Res, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { JwtService } from "@nestjs/jwt";
+import { Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+@Controller("auth")
 export class AuthController {
   constructor(private jwtService: JwtService) {}
 
-  @Get('login')
+  @Get("login")
   async login(@Res() response: Response): Promise<any> {
     // do username and password, typically
-    const userID = 'dummyuser'; // if userID was userDiD we'd need a DiD resolution method
+    const userID = uuidv4(); // if userID was userDiD we'd need a DiD resolution method
     const payload = { userID: userID }; // no sensitive data in the payload!!
     const token = this.jwtService.sign(payload);
+    const exp = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
 
     response
-      .cookie('access_token', token, {
+      .cookie("access_token", token, {
         httpOnly: true,
         secure: true,
         sameSite: true,
-        domain: 'localhost', // for now...
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        domain: "localhost", // for now...
+        expires: exp,
       })
       .send({
         success: true,
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+        expires: exp,
       });
   }
 
-  @Get('events')
-  @UseGuards(AuthGuard('jwt'))
+  @Get("events")
+  @UseGuards(AuthGuard("jwt"))
   async devices(): Promise<any> {
     const events = [
-      { date: new Date().getUTCDate(), description: 'Super event 1' },
-      { date: new Date().getUTCDate(), description: 'Super event 2' },
-      { date: new Date().getUTCDate(), description: 'Super event 3' },
-      { date: new Date().getUTCDate(), description: 'Super event 4' },
+      {
+        date: new Date().getUTCDate().toString(),
+        description: "Super event 1",
+      },
+      {
+        date: new Date().getUTCDate().toString(),
+        description: "Super event 2",
+      },
+      {
+        date: new Date().getUTCDate().toString(),
+        description: "Super event 3",
+      },
+      {
+        date: new Date().getUTCDate().toString(),
+        description: "Super event 4",
+      },
     ];
     return events;
   }
