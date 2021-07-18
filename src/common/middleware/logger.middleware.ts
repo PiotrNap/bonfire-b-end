a050d3a7194958de0da63515a0e97b6df2acee64
oldcryptogeek <kodex.data@gmail.com>
2021-07-17 18:18:07 -0600

    update logger

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
 export async function logger(req: Request, res: Response, next: NextFunction) {
-  console.log(req.rawHeaders);
-  console.log(res.statusCode)
+  console.log('----------------- Begin logger -----------------')
+  console.log(`Raw Headers     :: ` + req);
+  console.log(`Response Status :: ` + res.statusCode)
+  console.log('--------------------- next() -------------------')
   next();
+  console.log('----------------- End logger -------------------')
 };
