f8b5761a8762c5f47fe0486e30df9abaead6e026
oldcryptogeek <kodex.data@gmail.com>
2021-07-18 17:29:53 -0600

    update logger

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
 export async function logger(req: Request, res: Response, next: NextFunction) {
-  console.log('----------------- Begin logger -----------------')
-  console.log(`Raw Headers     :: ` + req);
-  console.log(`Response Status :: ` + res.statusCode)
+  console.log('***************** Begin logger -----------------')
+  console.log(`New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`)
+  //console.log(`Raw Headers     :: ${req.rawHeaders.toString()}`);
   console.log('--------------------- next() -------------------')
   next();
-  console.log('----------------- End logger -------------------')
+  console.log('----------------- End logger *******************')
 };
