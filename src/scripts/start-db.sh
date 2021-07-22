80abfe02f360ca2d6d6f78b3f14b22c28887fc81
oldcryptogeek <kodex.data@gmail.com>
2021-07-22 02:01:04 -0600

    rtf post-install m

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -7,21 +7,21 @@ PW="c0n3xd@ta!";
 DB="conexdb";
 
 # this will remove ALL containers from your system... DO NOT USE UNLESS YOU UNDERSTAND
-#sudo docker container stop $(sudo docker container ls -aq)
-#sudo docker container prune -f
+docker container stop $(docker container ls -aq)
+docker container prune -f
 
 echo "echo stop & remove old docker [$CONTAINER] and starting new fresh instance of [$CONTAINER]"
-( sudo docker kill $CONTAINER || :) && \
-  ( sudo docker rm $CONTAINER || :) && \
-   sudo docker run --name $CONTAINER -e POSTGRES_PASSWORD=$PW \
+( docker kill $CONTAINER || :) && \
+  ( docker rm $CONTAINER || :) && \
+   docker run --name $CONTAINER -e POSTGRES_PASSWORD=$PW \
   -e PGPASSWORD=$PW \
   -p 5432:5432 \
   -d postgres
 
 # wait for pg to start
 echo "sleep wait for pg-server [$CONTAINER] to start";
-sleep 12; # you may need to increase the sleep period if you get a psql error complaining that you can't connect to the server.
+sleep 20; # you may need to increase the sleep period if you get a psql error complaining that you can't connect to the server.
 
 # create the db 
-echo "CREATE DATABASE $DB ENCODING 'UTF-8';" |  sudo docker exec -i $CONTAINER psql -U postgres
-echo "\l" |  sudo docker exec -i $CONTAINER psql -U postgres
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" |  docker exec -i $CONTAINER psql -U postgres
+echo "\l" |  docker exec -i $CONTAINER psql -U postgres
