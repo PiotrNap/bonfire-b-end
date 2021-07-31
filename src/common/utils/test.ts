578003cfed3dfba0ca55e8d07d129b685c75b55e
oldcryptogeek <kodex.data@gmail.com>
2021-07-30 19:29:58 -0600

    utils

---
+++ src/common/utils/test.ts
@@ -0,0 +1,104 @@
+import {Address, hd} from "cardano-js";
+import { range } from "./utils";
+function thisIsATest() {
+  let r = range(1000000,10000000);
+  for (let i of r) {
+    let r = Address.Util.isAddress(
+      "addr_test1vqdaezgllu2g7lnkx60y7xyea4mvutxtgr49w25hrv6wfks9qn5ss",
+      i
+    );
+    console.log(i)
+    if (r === true) {return i} else { continue };
+  }
+}
+hd.gener
+console.log(thisIsATest());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(dando.Currency.Util.lovelacesToAda("1213215158778"))
+
+// import axios from "axios";
+
+// function getLatestBlockHeight() {
+//   return axios
+//     .get(
+//       `https://blockscout.com/etc/mainnet/api?module=block&action=eth_block_number`
+//     )
+//     .then((r) => {
+//       return r.data.result;
+//     })
+//     .then((r) => {
+//       return parseInt(r);
+//     });
+// }
+
+// function getSupply() {
+//   return axios
+//     .get(`https://blockscout.com/etc/mainnet/api?module=stats&action=ethsupply`)
+//     .then((r) => {
+//       return r.data.result;
+//     });
+// }
+
+// function getLatestInfo(blockNumber) {
+//   return axios
+//     .get(
+//       `https://blockscout.com/etc/mainnet/api?module=block&action=getblockreward&blockno=${blockNumber}`
+//     )
+//     .then(async (r) => {
+//       return await r.data.result;
+//     })
+//     .then(async (r) => {
+// let obj = {
+//          blockMiner: r.blockMiner,
+//          blockNumber: r.blockNumber,
+//          blockReward: r.blockReward,
+//          timestamp: r.timeStamp,
+//          uncleReward: r.uncleInclusionReward,
+//          uncles: r.uncles,
+//        };
+//        return obj;
+//      })}
+
+// async function execute() {
+//   console.log(await getLatestBlockHeight());
+//   console.log(await getSupply());
+//   console.log(await getLatestInfo(await getLatestBlockHeight()));
+// }
+
+// execute();
---
+++ src/common/utils/utils.ts
@@ -0,0 +1,67 @@
+export * as cbor from 'cbor'
+export * as bn from 'bignumber.js'
+
+export const range = (s, e) =>
+  e > s
+    ? Array(e - s + 1)
+        .fill(0)
+        .map((x, y) => y + s)
+    : Array(s - e + 1)
+        .fill(0)
+        .map((x, y) => -y + s);
+export const isoTime = new Date(Math.floor(Date.now())).toISOString();
+export const expiry = new Date(Math.floor(Date.now()) + 1000 * 3600);
+export const expirationTime = expiry.toLocaleTimeString();
+export const expirationDate = expiry.toLocaleDateString();
+export const localTime = new Date(Math.floor(Date.now())).toLocaleTimeString();
+export const localDate = new Date(Math.floor(Date.now())).toLocaleDateString();
+export const utcTime = new Date(Math.floor(Date.now())).toUTCString();
+export const argCounter = (...i) => {
+  let argCounter = 0;
+  for (let acc of i) argCounter++;
+  return argCounter;
+};
+export const elementCounter = (i) => {
+  let elemCounter = 0;
+  for (let acc of i) elemCounter++;
+  return elemCounter;
+};
+export const sum = (i: number[]) => {
+  return i.reduce((a, b) => a + b, 0);
+};
+
+
+
+
+export function stripWords(i) {
+  for (let x of i) {}
+  i = i.replace('.','');
+  i = i.replace(',','');
+  i = i.replace(':','');
+  i = i.replace(';','');
+  return i.split(' ')
+}
+export function stripSentence(i) {
+  let re = /(\.)/;
+  return i.split(re).filter(i=>i!=''&& i != '.')
+}
+export function stripValues(r) {
+  let res = []
+  for (let i of r) {
+    res.push(i);
+  }
+  return res
+}
+export function stripType(r) {
+  let res = [];
+  for (let i of r) {
+    // console.log(typeof(i))
+    res.push(typeof(i));
+  }
+  return res
+}
+
+
+
+const c = () => {
+}
