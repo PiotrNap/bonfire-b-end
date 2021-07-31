c9c20d81eb294834cbc1ae6678a7212ceae62ebb
oldcryptogeek <kodex.data@gmail.com>
2021-07-30 19:30:37 -0600

    logging

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,19 +1,31 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 import { v4 as uuidv4 } from "uuid";
 let eventID = uuidv4();
-const currentTime = Math.floor(Date.now());
+import { isoTime, localTime, utcTime, localDate } from "../utils/utils";
 
 export async function logger(req: Request, res: Response, next: NextFunction) {
-  console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-  console.log(`${eventID} initiated logging event @ ${new Date(currentTime + 1000 * 3600)}`)
+  const request = {
+    string : req.params[0],
+    path : req.path,
+    readableObject : req.readableObjectMode,
+    secureRequest : req.secure,
+
+  };
+  console.log(
+    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  );
+
+  console.log(
+    `${eventID} initiated logging event @ ${localTime} on ${localDate}`
+  );
   console.log(
     `New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`
   );
-  //console.log(`Raw Headers     :: ${req.rawHeaders.toString()}`);
-  console.log(`${eventID} calling next()`);
+  console.log(request.string);
   next();
-  console.log(`${eventID} sending response....`)
-  //  console.log(res)
+  console.log(res.statusMessage);
   console.log(`${eventID} complete ....!`);
-  console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+  console.log(
+    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  );
 }
