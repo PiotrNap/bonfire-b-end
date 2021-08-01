3c1467768f5c22a4fe53df64cdbd07344eda085c
oldcryptogeek <kodex.data@gmail.com>
2021-08-01 03:49:42 -0600

    cleanup merge

--- src/model/event.entity.ts
+++ src/model/event.entity.ts
@@ -1,46 +1,40 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
-
 @Entity()
 export class Event {
-    @PrimaryGeneratedColumn()
-    id: string; // this should be a uuidv4 or uuidv5
+  @PrimaryGeneratedColumn()
+  id: string; // this should be a uuidv4 or uuidv5
 
-    @Column('text')
-    description?: string;
+  @Column("text")
+  description?: string;
 
-    @Column()
-    place?: string
+  @Column()
+  place?: string;
 
-    @Column()
-    time?: string
+  @Column()
+  time?: string;
 
-    @Column()
-    url?: string
+  @Column()
+  url?: string;
 
-    @Column()
-    organizer?: string
+  @Column()
+  organizer?: string;
 
-    @Column()
-    attendees?: string
-
+  @Column()
+  attendees?: string;
 }
 export class Organizer {
-    @PrimaryGeneratedColumn()
-    id: string; // this should be a uuidv4 or uuidv5
+  @PrimaryGeneratedColumn()
+  id: string; // this should be a uuidv4 or uuidv5
 
-    @Column()
-    userName?: string;
+  @Column()
+  userName?: string;
 }
 
 export class Attendee {
-    @PrimaryGeneratedColumn()
-    id: string; // this should be a uuidv4 or uuidv5
+  @PrimaryGeneratedColumn()
+  id: string; // this should be a uuidv4 or uuidv5
 
-    @Column()
-    userName?: string;
-<<<<<<< HEAD
+  @Column()
+  userName?: string;
 }
-=======
-}
->>>>>>> 4876ce8c00662e1c4b84e8afb367f18a042dc447
