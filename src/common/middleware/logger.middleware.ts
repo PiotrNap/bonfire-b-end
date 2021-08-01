6e6471d697f388dd764f4b2650af6f5d6bee0954
oldcryptogeek <kodex.data@gmail.com>
2021-08-01 03:36:19 -0600

    update logger middleware

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,31 +1,21 @@
-import { Request, Response, NextFunction, response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 let eventID = uuidv4();
-import { isoTime, localTime, utcTime, localDate } from "../utils/utils";
+import { unixTime, localTimeStamp } from "../utils/utils";
+
+let bob = localTimeStamp
 
 export async function logger(req: Request, res: Response, next: NextFunction) {
-  const request = {
-    string : req.params[0],
-    path : req.path,
-    readableObject : req.readableObjectMode,
-    secureRequest : req.secure,
-
-  };
-  console.log(
-    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-  );
-
-  console.log(
-    `${eventID} initiated logging event @ ${localTime} on ${localDate}`
-  );
-  console.log(
-    `New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`
-  );
-  console.log(request.string);
+  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  console.log(eventID + ":" + localTimeStamp());
+  console.log()
+//  console.log(req.rawHeaders)
+  console.log(`New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`);
+  
+  // done logging, call next
   next();
-  console.log(res.statusMessage);
-  console.log(`${eventID} complete ....!`);
-  console.log(
-    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-  );
+
+  // all done ;)
+  console.log(`${eventID}:${localTimeStamp()}`);
+  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 }
