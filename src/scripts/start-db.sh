ec1c5d0196e3914f8066237e2e6da89fb0233fb9
oldcryptogeek <kodex.data@gmail.com>
2021-08-01 03:37:11 -0600

    v0.0.3 start-db.sh

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 set -e
 
@@ -5,9 +6,9 @@ set -e
 CONTAINER="conex-db-container"
 PW="c0n3xd@ta!";
 DB="conexdb";
-
+USER=$(whoami)
 # this will remove ALL containers from your system... DO NOT USE UNLESS YOU UNDERSTAND
-docker container stop $(docker container ls -aq)
+#docker container stop $(docker container ls -aq)
 docker container prune -f
 
 echo "echo stop & remove old docker [$CONTAINER] and starting new fresh instance of [$CONTAINER]"
@@ -15,12 +16,13 @@ echo "echo stop & remove old docker [$CONTAINER] and starting new fresh instance
   ( docker rm $CONTAINER || :) && \
    docker run --name $CONTAINER -e POSTGRES_PASSWORD=$PW \
   -e PGPASSWORD=$PW \
-  -p 5432:5432 \
+  -p 5435:5435 \
   -d postgres
 
 # wait for pg to start
 echo "sleep wait for pg-server [$CONTAINER] to start";
 sleep 20; # you may need to increase the sleep period if you get a psql error complaining that you can't connect to the server.
+
 
 # create the db 
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" |  docker exec -i $CONTAINER psql -U postgres
