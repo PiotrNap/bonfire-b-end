49732365e2e6cd7b839ef91a1969ded4a971472c
oldcryptogeek <kodex.data@gmail.com>
2021-08-18 14:00:26 -0600

    update database start script

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -8,18 +8,22 @@ DB="conexdb"
 
 mkdir -p $(pwd)/devDb
 echo "echo stop & remove old docker [$CONTAINER] and starting new fresh instance of [$CONTAINER]"
-# (docker kill $CONTAINER || :) &&
-#   (docker rm $CONTAINER || :) &&
+(docker kill $CONTAINER || :) &&
+  (docker rm $CONTAINER || :) &&
   docker ps | grep $CONTAINER || (
-    docker run --mount type=bind,source="$(pwd)"/devDb,target=/var/lib/postgresql/data --name $CONTAINER -e POSTGRES_PASSWORD=$PW \
+    docker run \
+      -d \
+      -p 5435:5432 \
+      -e POSTGRES_PASSWORD=$PW \
       -e PGPASSWORD=$PW \
-      -p 5435:5435 \
-      -d postgres
+      --name $CONTAINER \
+      --mount type=bind,source="$(pwd)"/devDb,target=/var/lib/postgresql/data \
+      postgres
 )
 
 # wait for pg to start
 echo "sleep wait for pg-server [$CONTAINER] to start"
-sleep 5 # you may need to increase the sleep period if you get a psql error complaining that you can't connect to the server.
+sleep 20 # you may need to increase the sleep period if you get a psql error complaining that you can't connect to the server.
 
 # create the db
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $CONTAINER psql -U postgres
