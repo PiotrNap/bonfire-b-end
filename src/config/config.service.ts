6cc30c37d289d56a6de900d62f2717654a0cbfff
oldcryptogeek <kodex.data@gmail.com>
2021-08-25 17:19:15 -0600

    remove .env comment from config

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,10 +1,9 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv'
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 class ConfigService {
-
-  constructor(private env: { [k: string]: string | undefined }) { }
+  constructor(private env: { [k: string]: string | undefined }) {}
 
   private getValue(key: string, throwOnMissing = true): string {
     const value = this.env[key];
@@ -16,62 +15,49 @@ class ConfigService {
   }
 
   public ensureValues(keys: string[]) {
-    keys.forEach(k => this.getValue(k, true));
+    keys.forEach((k) => this.getValue(k, true));
     return this;
   }
 
   public getPort() {
-    return this.getValue('PORT', true);
+    return this.getValue("PORT", true);
   }
 
   public isProduction() {
-    const mode = this.getValue('MODE', false);
-    return mode != 'DEV';
+    const mode = this.getValue("MODE", false);
+    return mode != "DEV";
   }
-/**
- * Please update your .env with the following values.  Set password and database to match values in /scripts/start-db.sh
- * POSTGRES_HOST=127.0.0.1
- * POSTGRES_PORT=5432
- * POSTGRES_USER=postgres
- * POSTGRES_PASSWORD=c0n3xd@ta!
- * POSTGRES_DATABASE=conexdb
- * PORT=3000
- * MODE=DEV
- * RUN_MIGRATIONS=<0|1>
- */
   public getTypeOrmConfig(): TypeOrmModuleOptions {
     return {
-      type: 'postgres',
+      type: "postgres",
 
-      host: this.getValue('POSTGRES_HOST'),
-      port: parseInt(this.getValue('POSTGRES_PORT')),
-      username: this.getValue('POSTGRES_USER'),
-      password: this.getValue('POSTGRES_PASSWORD'),
-      database: this.getValue('POSTGRES_DATABASE'),
+      host: this.getValue("POSTGRES_HOST"),
+      port: parseInt(this.getValue("POSTGRES_PORT")),
+      username: this.getValue("POSTGRES_USER"),
+      password: this.getValue("POSTGRES_PASSWORD"),
+      database: this.getValue("POSTGRES_DATABASE"),
 
-      entities: ['dist/model/*.entity.js'],
+      entities: ["dist/model/*.entity.js"],
 
-      migrationsTableName: 'migration',
+      migrationsTableName: "migration",
 
-      migrations: ['dist/migration/*.js'],
+      migrations: ["dist/migration/*.js"],
 
       cli: {
-        migrationsDir: 'src/migration',
+        migrationsDir: "src/migration",
       },
 
       ssl: this.isProduction(),
     };
   }
-
 }
 
-const configService = new ConfigService(process.env)
-  .ensureValues([
-    'POSTGRES_HOST',
-    'POSTGRES_PORT',
-    'POSTGRES_USER',
-    'POSTGRES_PASSWORD',
-    'POSTGRES_DATABASE'
-  ]);
+const configService = new ConfigService(process.env).ensureValues([
+  "POSTGRES_HOST",
+  "POSTGRES_PORT",
+  "POSTGRES_USER",
+  "POSTGRES_PASSWORD",
+  "POSTGRES_DATABASE",
+]);
 
 export { configService };
