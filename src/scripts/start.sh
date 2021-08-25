f8a01070530c0587f233cb49b6ed4fb5b8e0c4ff
oldcryptogeek <kodex.data@gmail.com>
2021-08-25 15:27:12 -0600

    update scripts

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -1,5 +1,5 @@
-#!/bin/bashset -e
-set -xif [ "$RUN_MIGRATIONS" ]; then
+#!/bin/sh
+set -exif [ "$RUN_MIGRATIONS" ]; then
   echo "RUNNING MIGRATIONS";
   npm run typeorm:migration:run
 fiecho "START SERVER";
