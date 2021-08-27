5f8da64dc367f74f5e7bdcbe0aa44bae5608ed90
oldcryptogeek <kodex.data@gmail.com>
2021-08-26 22:48:02 -0600

    add discord passport strategy

---
+++ src/auth/strategies/discord.strategy.ts
@@ -0,0 +1,26 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { Strategy } from "passport-oauth2";
+
+@Injectable()
+export class DiscordStrategy extends PassportStrategy(Strategy, "discord") {
+  http = require("http");
+  constructor() {
+    super({
+      authorizationURL: null,
+      tokenURL: null,
+      clientID: null,
+      clientSecret: null,
+      callbackURL: null,
+      scope: null,
+    });
+  }
+
+  async validate(accessToken: string): Promise<any> {
+    const { data } = await this.http
+      .get("https://discordapp.com/api/users/@me", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .toPromise();
+  }
+}
