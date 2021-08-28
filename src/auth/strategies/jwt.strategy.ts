e9f4aeae4d27d58c460dfb31205e5d5f979cc974
oldcryptogeek <kodex.data@gmail.com>
2021-08-27 19:44:58 -0600

    update jwt.strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,24 +1,3 @@
-// import { Injectable } from "@nestjs/common";
-// import { PassportStrategy } from "@nestjs/passport";
-// import { ExtractJwt, Strategy } from "passport-jwt";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-// @Injectable()
-// export class JwtStrategy extends PassportStrategy(Strategy) {
-//   constructor() {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       ignoreExpiration: false,
-//       secretOrKey: process.env.JWT_SECRET,
-//     });
-//   }
-
-//   async validate(payload: any) {
-//     return { userId: payload.sub, username: payload.username }; //other: payload.x
-//   }
-// }
-
-
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
