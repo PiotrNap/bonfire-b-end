7e79dfa6e14415b035b25499808da8db448c0cf0
oldcryptogeek <kodex.data@gmail.com>
2021-08-27 19:44:26 -0600

    update auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
-
+import { LocalStrategy } from './strategies/local.strategy'
 @Module({
   imports: [
     UsersModule,
@@ -19,12 +19,12 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: process.env.EXPIRESIN,
+        expiresIn: process.env.EXPIRES_IN,
       },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
   exports: [PassportModule, JwtModule],
 })
 export class AuthModule {}
