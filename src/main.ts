6c623744aa5c4d94d9816df7a6410eac999fc5c4
Piotr Napierala <piotr.napierala94@gmail.com>
2021-08-31 19:19:47 +0200

    Enable CORS in maint.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import * as dotenv from 'dotenv';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  // @TODO: remove before deploying
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
