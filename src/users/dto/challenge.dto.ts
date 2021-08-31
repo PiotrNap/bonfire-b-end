5b9f018779f14367ba883b82ba1f9112acd79b89
Piotr Napierala <piotr.napierala94@gmail.com>
2021-08-31 19:17:48 +0200

    Replace bcrypt with sha256 in ChallengeDTO

--- src/users/dto/challenge.dto.ts
+++ src/users/dto/challenge.dto.ts
@@ -1,10 +1,10 @@
 import { IsNotEmpty } from "class-validator";
-import bcrypt from 'bcrypt'
+import { sha256 } from "js-sha256";
 export class ChallengeDTO {
   constructor(userid: string) {
-    let time = new Date().getTime()
-    let hash = bcrypt.hash(userid+time, 10)
-    this.challengeString = `${userid}-${time}-${hash}`
+    let time = new Date().getTime();
+    let hash = sha256(userid + time + process.env.JWT_SECRET);
+    this.challengeString = `${userid}-${time}-${hash}`;
   }
   @IsNotEmpty()
   challengeString: string; // Hash of userid and timestamp
