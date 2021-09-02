ab3facea2f6d0bb9eb2edee67dc1c4a059ba0160
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-02 12:20:05 +0200

    Update 'auth/register' controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,45 +2,40 @@ import {
   Controller,
   Body,
   Post,
-  HttpException,
-  HttpStatus,
-  UsePipes,
   Get,
   Req,
   UseGuards,
   Param,
 } from "@nestjs/common";
 import { CreateUserDto } from "../users/dto/user.create.dto";
-import { RegistrationStatus } from "./interfaces/registration-status.interface";
 import { AuthService } from "./auth.service";
 import { LoginStatus } from "./interfaces/login-status.interface";
 import { JwtPayload } from "./interfaces/payload.interface";
 import { AuthGuard } from "@nestjs/passport";
 import { ChallengeResponseDTO } from "src/users/dto/challenge-response.dto";
 import { ChallengeDTO } from "src/users/dto/challenge.dto";
+import { UsersService } from "src/users/users.service";
+import { UserDto } from "src/users/dto/user.dto";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService
+  ) {}
 
   @Post("register")
   public async register(
     @Body() createUserDto: CreateUserDto
-  ): Promise<RegistrationStatus> {
-    const result: RegistrationStatus = await this.authService.register(
-      createUserDto
-    );
-
-    if (!result.success) {
-      throw new HttpException(result.message, HttpStatus.BAD_REQUEST);
-    }
+  ): Promise<UserDto> {
+    const result: UserDto = await this.usersService.registerUser(createUserDto);
 
     return result;
   }
 
   @Get(":id/challenge")
-  public async challenge(@Param() params): Promise<ChallengeDTO> {
-    return await this.authService.challenge(params.id);
+  public async challenge(@Param() param: string): Promise<ChallengeDTO> {
+    return await this.authService.challenge(param);
   }
 
   @Post("login")
