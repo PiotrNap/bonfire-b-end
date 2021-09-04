29ce0d249d77d5d9863e93ff24ca162f0a929916
Piotr Napierala <piotr.napierala94@gmail.com>
2021-09-04 19:28:12 +0200

    Update user.dto & user.create.dto

--- src/users/dto/user.create.dto.ts
+++ src/users/dto/user.create.dto.ts
@@ -1,15 +1,25 @@
-import { IsNotEmpty, IsEmail } from "class-validator";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateUserDto {
-  constructor(name: string, username: string, publicKey: string, id: string) {
+  constructor(
+    name: string,
+    username: string,
+    publicKey: string,
+    id: string,
+    profileType: string
+  ) {
     this.name = name;
     this.username = username;
     this.publicKey = publicKey;
     this.id = id;
+    this.profileType = profileType;
   }
 
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  profileType: string;
 
   @IsNotEmpty()
   username: string;
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty } from "class-validator";
 export class UserDto {
   id: string;
   username: string;
   publicKey: string;
+  profileType: string;
   createdOn?: Date;
+  currChallenge?: string | null;
   calendarToken?: string | null;
   refreshToken?: string | null;
 }
