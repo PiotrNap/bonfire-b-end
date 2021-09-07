0bb99e015b9890a337210a81f0677a057103ff43
Randall <randall@gimbalabs.io>
2021-09-07 12:43:29 -0700

    resolve conflicts

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,7 @@
 import { ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
-<<<<<<< Updated upstream
-import { IS_PUBLIC_KEY } from "src/common/decorators/public.decorator";
-=======
-import { NO_JWT_AUTH } from "src/common/decorators/public.decorator";
->>>>>>> Stashed changes
+import { NO_JWT_AUTH } from "common/decorators/public.decorator";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
@@ -15,25 +11,15 @@ export class JwtAuthGuard extends AuthGuard("jwt") {
 
   canActivate(context: ExecutionContext) {
     try {
-<<<<<<< Updated upstream
-      const isPublic = this.reflector.getAllAndOverride<boolean>(
-        IS_PUBLIC_KEY,
-        [context.getHandler(), context.getClass()]
-      );
-=======
       const isPublic = this.reflector.getAllAndOverride<boolean>(NO_JWT_AUTH, [
         context.getHandler(),
         context.getClass(),
       ]);
->>>>>>> Stashed changes
 
       if (isPublic) {
         return true;
       }
-<<<<<<< Updated upstream
       console.log(context);
-=======
->>>>>>> Stashed changes
       return super.canActivate(context);
     } catch (e) {
       console.error(e);
--- src/common/decorators/public.decorator.ts
+++ src/common/decorators/public.decorator.ts
@@ -1,9 +1,4 @@
 import { SetMetadata } from "@nestjs/common";
 
-<<<<<<< Updated upstream
-export const IS_PUBLIC_KEY = "isPublic";
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
-=======
 export const NO_JWT_AUTH = "isPublic";
 export const Public = () => SetMetadata(NO_JWT_AUTH, true);
->>>>>>> Stashed changes
