6f34d383d88fb6e83e47f427f3f27b1913149faf
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-06 19:27:58 +0200

    Add google interface file

---
+++ src/auth/interfaces/google.interface.ts
@@ -0,0 +1,18 @@
+export class GoogleCalendarScopes {
+  Calendar = "https=//www.googleapis.com/auth/calendar";
+  CalendarEvents = "https=//www.googleapis.com/auth/calendar.events";
+  CalendarEventsRead =
+    "https=//www.googleapis.com/auth/calendar.events.readonly";
+  CalendarRead = "https=//www.googleapis.com/auth/calendar.readonly";
+  CalendarSettings =
+    "https=//www.googleapis.com/auth/calendar.settings.readonly";
+}
+
+export interface Credentials {
+  refresh_token?: string | null;
+  expiry_date?: number | null;
+  access_token?: string | null;
+  token_type?: string | null;
+  id_token?: string | null;
+  scope?: string;
+}
