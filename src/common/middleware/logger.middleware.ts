258371f166368118516c99dca20a6987f5ce0247
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-25 17:51:49 +0200

    [logger-middleware] log req url and body

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -3,19 +3,26 @@ import { v4 as uuidv4 } from "uuid";
 let eventID = uuidv4();
 import { unixTime, localTimeStamp } from "../utils";
 
-let bob = localTimeStamp
+let bob = localTimeStamp;
 
 export async function logger(req: Request, res: Response, next: NextFunction) {
-  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  console.log(
+    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  );
   console.log(eventID + ":" + localTimeStamp());
-  console.log()
-//  console.log(req.rawHeaders)
-  console.log(`New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`);
-  
+  console.log("Requsted URL: ", req.originalUrl);
+  console.log(req.body);
+  //  console.log(req.rawHeaders)
+  console.log(
+    `New ${req.socket.remoteFamily} request from [${req.socket.remoteAddress}]:${req.socket.remotePort}`
+  );
+
   // done logging, call next
   next();
 
   // all done ;)
   console.log(`${eventID}:${localTimeStamp()}`);
-  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  console.log(
+    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  );
 }
