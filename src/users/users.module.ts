5eb03151e7a87cbb51ef125d67d7a18e1fa9237c
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-25 17:44:13 +0200

    Insert AttendeeEntity to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,12 @@ import { UserEntity } from "../model/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersController } from "./users.controller";
 import { OrganizerEntity } from "src/model/organizer.entity";
+import { AttendeeEntity } from "src/model/attendee.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, OrganizerEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, OrganizerEntity, AttendeeEntity]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
