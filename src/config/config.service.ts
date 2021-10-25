0a39aba68d3669fa5d4adc1f0b72d5786c88a4d1
Piotr Napierala <piotr.napierala94@gmail.com>
2021-10-25 17:51:03 +0200

    [config-service] add logging=true

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -30,27 +30,21 @@ class ConfigService {
   public getTypeOrmConfig(): TypeOrmModuleOptions {
     return {
       type: "postgres",
-
       host: this.getValue("POSTGRES_HOST"),
       port: parseInt(this.getValue("POSTGRES_PORT")),
       username: this.getValue("POSTGRES_USER"),
       password: this.getValue("POSTGRES_PASSWORD"),
       database: this.getValue("POSTGRES_DATABASE"),
-
       //@TODO: remove for production
       synchronize: true,
-
       entities: ["dist/model/*.entity.js"],
-
       migrationsTableName: "migration",
-
       migrations: ["dist/migration/*.js"],
-
       cli: {
         migrationsDir: "src/migration",
       },
-
       ssl: this.isProduction(),
+      logging: true,
     };
   }
 }
