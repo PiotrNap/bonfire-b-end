e4eb986198eb279905c44cba047ee58356b16c45
Piotr Napierala <piotr.napierala94@gmail.com>
2022-03-30 13:27:45 +0200

    add migration to add 'fromData','toDate' to events

---
+++ src/migration/1635012080049-migration.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migration1635012080049 implements MigrationInterface {
+    name = 'migration1635012080049'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "event_entity" ADD "fromDate" TIMESTAMP WITH TIME ZONE NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "event_entity" ADD "toDate" TIMESTAMP WITH TIME ZONE NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "event_entity" DROP COLUMN "toDate"`);
+        await queryRunner.query(`ALTER TABLE "event_entity" DROP COLUMN "fromDate"`);
+    }
+
+}
