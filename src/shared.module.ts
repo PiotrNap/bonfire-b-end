b4ded46d2050667e6130a0568d835ad2f5550a5b
Piotr Napierala <piotr.napierala94@gmail.com>
2022-03-30 13:27:31 +0200

    add shared module

---
+++ src/shared.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./auth/strategies/jwt.strategy";
+
+@Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: "jwt",
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "24h" },
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
+})
+export class SharedModule {}
