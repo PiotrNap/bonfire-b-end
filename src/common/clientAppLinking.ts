d212808b0de67171987a97c25b7e99f08b112a24
Piotr Napierala <piotr.napierala94@gmail.com>
2022-03-30 13:22:55 +0200

    Add enums for deep linking & file-upload interface

---
+++ src/common/clientAppLinking.ts
@@ -0,0 +1,7 @@
+/**
+ * paths to build our callback urls for an in-app browser redirection
+ */
+export enum DEEP_LINKING_PATHS {
+  "Available Days Selection" = "available-days-selection/",
+  "Available Event Days Selection" = "available-event-days-selection/",
+}
---
+++ src/common/lib/interfaces.ts
@@ -0,0 +1,8 @@
+export interface FileUpload {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  buffer: string;
+  size: number;
+}
